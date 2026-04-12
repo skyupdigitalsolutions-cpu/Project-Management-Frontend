4bbe76cd0ba25fcbb3b25c19e259ded62ad7a347
Shashikant-S-Bilgundi <shashibilgundi@gmail.com>
2026-04-12 14:39:15 +0530

    updated url updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://project-management-backend-gvpy.onrender.com',
         changeOrigin: true,
       }
     }
