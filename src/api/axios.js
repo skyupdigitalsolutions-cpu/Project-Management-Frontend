7ade42b811d889a8a2a95026930bad1d42305222
SKYUP DIGITAL SOLUTIONS <skyupdigitalsolutions@gmail.com>
2026-04-13 10:19:22 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-import axios from 'axios'
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
   headers: { 'Content-Type': 'application/json' },
